9b5a4a87823e28acc425278ba88b9d6c5c4af969
ARTH SHARMA <arth89408@gmail.com>
2026-03-22 15:48:24 +0530

    Time: 15 ms (44.98%) | Memory: 49.9 MB (7.39%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,46 @@
+class pair{
+    int row;
+    int col;
+    int step;
+    pair(int row , int col ,int step ){
+        this.row=row;
+        this.col=col;
+        this.step=step;
+    }
+}
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m=mat.length;
+        int n=mat[0].length;
+        int [][] vis=new int[m][n];
+        int[][] ans=new int [m][n];
+        Queue<pair> q=new LinkedList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    q.add(new pair(i,j,0));
+                    vis[i][j]=1;
+                }
+            }
+        }
+        int[] drow={-1,0,1,0};
+        int[] dcol={0,1,0,-1};
+        while(!q.isEmpty()){
+            int r=q.peek().row;
+            int c=q.peek().col;
+            int s=q.peek().step;
+            q.remove();
+            ans[r][c]=s;
+            for(int k=0;k<4;k++){
+                int nrow= r+ drow[k];
+                int ncol= c+ dcol[k];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0){
+                    q.add(new pair(nrow,ncol,s+1));
+                    vis[nrow][ncol]=1;
+
+                }
+            }
+        }
+        return ans;
+    }
+}
