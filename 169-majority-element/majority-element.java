b58e3414ecbdd52eca2b0559fe4e6feee63c8d6f
ARTH SHARMA <arth89408@gmail.com>
2026-04-17 09:00:49 +0530

    Time: 17 ms (15.89%) | Memory: 52.6 MB (82.71%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+        for(int i=0;i< nums.length;i++){
+            mpp.put(nums[i],mpp.getOrDefault(nums[i],0)+1);
+            if(mpp.get(nums[i])>nums.length/2){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
